02bc253f1b164de90e7abe5e26a1355dfd1bb2c4
garciasealvaro <garciasealvaro@gmail.com>
2026-02-07 10:57:31 +0100

    Update convert_code.m

--- static/mmci-cli/lib/convert_code.m
+++ static/mmci-cli/lib/convert_code.m
@@ -27,34 +27,33 @@ lines_change{end+1}= "M_.param_names{i}";
 
 % copy paste original code
 %copyfile(name+".mod",name+"_orig.mod");
-        
 % Read the mod file as a cell with element being one line of code
-code=importdata(name+".mod", '\n');
-code=string(code);            
+% --- FIX FOR OCTAVE/LINUX START ---
+if exist([name, '.mod'], 'file')
+    raw_text = fileread([name, '.mod']); % Read all the file as an only string
+    code = strsplit(raw_text, {'\n', '\r'}, 'CollapseDelimiters', false); % Cut by line breaks
+    code = code(:);                      % Ensure that it is a column vector.
+else
+    error(['The file ' name '.mod cannot be found.']);
+end
+% --- FIX FOR OCTAVE/LINUX END ---
 % Look for the sequences where to change code
-for j=1:size(sequence_change,2)
-    do=1;
-    while do==1
-        do=0;
-        index_sequ_start = strfind(code,sequence_change{1,j});
-        index_sequ_start = find(~cellfun(@isempty,index_sequ_start)); 
-        if isempty(index_sequ_start)==false
-            code(index_sequ_start) = strrep(code(index_sequ_start),sequence_change{1,j},lines_change{1,j});
-            %code = vertcat(code(1:index_sequ_start-1),lines_change{1,j},code(index_sequ_start+1:end));
-            check=1;
-            do=1;
-        end
-    end
+% Look for the sequences where to change code
+for j = 1:size(sequence_change, 2)
+    % Vectorized replacement: applies the change to all lines at once.
+    % This replaces the manual 'while' loop and index searching.
+    code = strrep(code, sequence_change{1,j}, lines_change{1,j});
 end
 
+% Set check to 1 to indicate the conversion process ran
+check = 1;
 
-                
 % create a mod file with the converted code
-filename = name+".mod";
+filename = [name, '.mod'];
 fid = fopen(filename,'wt');
-for k = 1:size(code)
-    fprintf(fid, '%s \n', code(k));
+for k = 1:length(code)
+fprintf(fid, '%s \n', code{k});
 end
-fclose(fid);   
+fclose(fid);
 
 cd(oldpath)
